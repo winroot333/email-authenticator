3a6d37628640673855ad66a6540c3e98b21ddb22
Mihail Vasilev <winroot123@gmail.com>
2026-03-26 14:31:41 +0300

    tmp compose kafka

---
+++ authentication-service/src/main/java/io/github/winroot33/authenticationservice/config/KafkaConfig.java
@@ -0,0 +1,21 @@
+package io.github.winroot33.authenticationservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    NewTopic createTopic() {
+        return TopicBuilder.name("client-notification-event-topic")
+                .partitions(3)
+                .replicas(3)
+                .configs(Map.of("min.insync.replicas", "2"))
+                .build();
+    }
+}
